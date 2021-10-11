3ae164cbe09ace224468f1c07a6d73c8c9d61737
Douglas S. Almeida <douglasbr@gmail.com>
2021-10-11 16:57:53 -0400

    Angular tour of heroes tutorial
    initial commit tour of heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 10, name: 'Bruxo' },
+  { id: 30, name: 'Messi' },
+  { id: 5, name: 'Zidane' },
+  { id: 1000, name: 'Pele' },
+  { id: 16, name: 'Jardel' },
+  { id: 7, name: 'Paulo Nunes' },
+  { id: 9, name: 'Ronaldo' },
+  { id: 11, name: 'Romario O Mito Faria' },
+  { id: 100, name: 'Diego' },
+  { id: 77, name: 'CR7' }
+];
